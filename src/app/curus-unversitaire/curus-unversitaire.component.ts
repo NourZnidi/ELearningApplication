137b004745ba2968ff809035a54d1c54b2bdee37
Znidi Nour <101046476+NourZnidi@users.noreply.github.com>
2024-07-03 00:23:50 +0100

    some modifications

--- src/app/curus-unversitaire/curus-unversitaire.component.ts
+++ src/app/curus-unversitaire/curus-unversitaire.component.ts
@@ -3,16 +3,36 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { HomeComponent } from '../home/home.component';
 import { FooterComponent } from '../footer/footer.component';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-curus-unversitaire',
   standalone: true,
-  imports: [RouterLink,RouterLinkActive,HomeComponent,FooterComponent,NavbarComponent,RouterOutlet],
+  imports: [CommonModule,RouterLink,RouterLinkActive,HomeComponent,FooterComponent,NavbarComponent,RouterOutlet],
   templateUrl: './curus-unversitaire.component.html',
   styleUrl: './curus-unversitaire.component.css'
 })
 export class CurusUnversitaireComponent {
+  isOpen = false;
+  isLicenceOpen = false;
+  isMaitriseOpen = false;
 
+  toggleDropdown() {
+    this.isOpen = !this.isOpen;
+  }
 
+  toggleLicenceDropdown() {
+    this.isLicenceOpen = !this.isLicenceOpen;
+    if (this.isLicenceOpen) {
+      this.isMaitriseOpen = false;
+    }
+  }
+
+  toggleMaitriseDropdown() {
+    this.isMaitriseOpen = !this.isMaitriseOpen;
+    if (this.isMaitriseOpen) {
+      this.isLicenceOpen = false;
+    }
+  }
 
 }
